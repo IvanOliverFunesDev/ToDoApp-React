b63bd2a4d5bccc8b93a54875ae461ded76bd2f3c
IvanOliverFunesDev <ioliverfunes@gmail.com>
2025-04-01 14:18:04 +0200

    feat: prueba useEffect para '/register'

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,4 +1,26 @@
+import { useEffect } from "react";
+import { register } from "../services/authService";
+
 export default function RegisterPage(){
-    return <h1>Register Page</h1>;
+    useEffect(()=>{
+        const probarRegistro = async () => {
+            try {
+                const resultado = await register({
+                    name: "pruebareact3",
+                    email: "pruebareact3@gmail.com",
+                    password: "12345678"
+                });
+                console.log("Registro exitoso", resultado);
+            } catch (error) {
+                console.error("❌ Error al registrar:", error.response?.data || error.message) 
+            }
+        };
+        probarRegistro()
+    },[]);
+    return(
+        <div>
+            <h2>Probando registro automático....</h2>
+        </div>
+    )
 }
 
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost/api/register';
+const API_URL = 'http://localhost/api';
 
 export const register = async (userData)=> {
     const response = await axios.post(`${API_URL}/register`, userData);
