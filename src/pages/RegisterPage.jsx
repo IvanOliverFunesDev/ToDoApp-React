1c488c51ad33cd3acb7d248517dc1faa28eb4152
IvanOliverFunesDev <ioliverfunes@gmail.com>
2025-04-02 10:17:22 +0200

    feat: añade formulario para '/register'

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,26 +1,33 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import { register } from "../services/authService";
+import { Form } from "react-router-dom";
+import './RegisterPageStyle.css'
 
-export default function RegisterPage(){
-    useEffect(()=>{
-        const probarRegistro = async () => {
-            try {
-                const resultado = await register({
-                    name: "pruebareact3",
-                    email: "pruebareact3@gmail.com",
-                    password: "12345678"
-                });
-                console.log("Registro exitoso", resultado);
-            } catch (error) {
-                console.error("❌ Error al registrar:", error.response?.data || error.message) 
-            }
-        };
-        probarRegistro()
-    },[]);
-    return(
-        <div>
-            <h2>Probando registro automático....</h2>
-        </div>
-    )
+export default function RegisterPage() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const data = await register({ name, email, password });
+            console.log("Registro exitoso:", data);
+        } catch (err) {
+            console.error("Error al registrar:", err.response?.data || err.message);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Registro</h2>
+            <input type="text" placeholder="Nombre" value={name} onChange={(e) => setName(e.target.value)} required />
+            <input type="email" placeholder="Correo electronico" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+
+            <button type="submit">Registrarse</button>
+        </form>
+    );
 }
 
