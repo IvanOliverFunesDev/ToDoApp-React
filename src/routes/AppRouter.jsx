178aa9007feebbe794bb1360e43ef56ce65986d4
IvanOliverFunesDev <ioliverfunes@gmail.com>
2025-04-02 13:39:34 +0200

    feat: crea formulario para '/login'

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RegisterPage from '../pages/RegisterPage'
+import LoginPage from "../pages/LoginPage";
+import NewTaskPage from "../pages/NewTaskPage"
 
 const router = createBrowserRouter([
     {
@@ -8,7 +10,11 @@ const router = createBrowserRouter([
     },
     {
         path: "/login",
-        element: <Login />
+        element: <LoginPage />
+    },
+    {
+        path: "/crearTask",
+        element: <NewTaskPage />
     }
 ])
 
